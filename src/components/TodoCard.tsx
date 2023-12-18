8ff5bc107eeb8dcebc423cab213a4af81278ab8e
Max <ppmax@bk.ru>
2023-12-19 01:13:51 +0300

    Todo item component added

---
+++ src/components/TodoCard.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { Todo } from '../entities/Todo';
+
+interface TodoProps {
+  todo: Todo;
+}
+
+const TodoCard = ({ todo }: TodoProps) => {
+  const [checked, setChecked] = useState(todo.completed);
+
+  return (
+    <div>
+      {todo.title}
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => setChecked(e.target.checked)}
+      />
+    </div>
+  );
+};
+
+export default TodoCard;
